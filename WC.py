0d08e850d37d02941523fe363ee436d97d461f3d
Gopika <75885964+Gopika-1091@users.noreply.github.com>
2021-01-08 12:53:22 +0530

    Update WC.py

--- WC.py
+++ WC.py
@@ -1,17 +1,10 @@
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 text = "If I could invent a superhero, I would invent one called Disappointment Panda. He'd wear a cheesy eye mask and a shirt that was way too small for his big panda belly, and his superpower would be to tell people harsh truths about themselves that they needed to hear but didn't want to accept. He would go door-to-door like a Bible salesman and ring doorbells and say things like, “Sure, making a lot of money makes you feel good, but it won't make your kids love you,” or “If you have to ask yourself if you trust your wife, then you probably don't,” or “What you consider ‘friendship’ is really just your constant attempts to impress people.” Then he'd tell the homeowner to have a nice day and saunter on down to the next house. It would be awesome. And sick. And sad. And uplifting. And necessary. After all, the greatest truths in life are usually the most unpleasant to hear. Disappointment Panda would be the hero that none of us would want but all of us would need. He'd be the proverbial vegetables to our mental diet of junk food. He'd make our lives better despite making us feel worse. He'd make us stronger by tearing us down, brighten our future by showing us the darkness. Listening to him would be like watching a movie where the hero dies in the end: you love it even more despite making you feel horrible, because it feels real. So while we're here, allow me to put on my Disappointment Panda mask and drop another unpleasant truth on you: We suffer for the simple reason that suffering is biologically useful."
-text = text.replace(" and","")
-text = text.replace(" And","")
-text = text.replace(" or","")
-text = text.replace(","," ")
-text = text.replace("."," ")
-text = text.replace(" He'd "," ")
-text = text.replace(" he'd "," ")
-text = text.replace(" don't","")
-text = text.replace(" won't","")
-text = text.replace(" us "," ")
+rem = [" and", " And", " or", " He'd", " he'd", " don't", " won't", " us ", "feel", "make"]
 
+for i in rem:
+    text = text.replace(i," ")
 
 
 def plot_cloud(wordcloud):
@@ -19,7 +12,6 @@ def plot_cloud(wordcloud):
     plt.imshow(wordcloud, interpolation = "bilinear") 
     plt.axis("off")
     plt.show()
-
 
 
 wordcloud = WordCloud(width = 3000,
